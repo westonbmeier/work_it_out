749409a480c8b9cb1375f5c10d24e54d8607a6b6
westonbmeier <westonbmeier@gmail.com>
2020-04-12 12:42:20 -0600

    api route

---
+++ routes/api-routes.js
@@ -0,0 +1,45 @@
+const router = require("express").Router();
+const path = require("path");
+const Workout = require("../models/workoutSchema.js");
+
+router.get("/exercise", (req, res) => {
+    res.sendFile(path.join(__dirname, "/../public/exercise.html"));
+});
+
+router.get("/stats", (req, res) => {
+    res.sendFile(path.join(__dirname, "/../public/stats.html"));
+});
+
+router.get("/api/workouts", (req, res) => {
+    Workout.find({})
+        .sort({day: -1})
+        .then(function(data){
+            res.json(data[0]);
+        })
+        .catch(err => console.log(err));
+});
+
+router.post("/api/workouts", (req, res) => {
+    let workout = new Workout;
+    workout.save( (err, result) => {
+        if (err) {
+            throw err;
+        }
+        res.json({"inserted": result});
+    });
+});
+
+router.put("/api/workouts/:id", (req, res) => {
+    let id = req.params.id;
+    let exercise = req.body;
+    Workout.findByIdAndUpdate({_id: id}, {$push: {exercises: exercise}})
+        .then(result => res.json({"result:": result}))
+        .catch(err => console.log(err))
+});
+
+router.get("/api/workouts/range", (req, res) => {
+    Workout.find({})
+        .then(data => res.json(data));
+});
+
+module.exports = router;
