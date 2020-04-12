67e67ba187b495bdb0d0cb69be909903102cc3b6
westonbmeier <westonbmeier@gmail.com>
2020-04-12 12:49:24 -0600

    connected to mongoose

--- server.js
+++ server.js
@@ -8,3 +8,8 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
